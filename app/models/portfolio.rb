ca72abb5113518a4925050026451b7a2dee9d5fe
Rafael Garcia <rafagarciafdz@gmail.com>
2019-02-11 15:20:57 -0600

    Before the scopes, implemented image defaults urls for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,15 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+	after_initialize :set_defaults
 
 	def self.angular
 		where(subtitle: 'Angular')
 	end
 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')} 
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/350x150"
+	end
 end

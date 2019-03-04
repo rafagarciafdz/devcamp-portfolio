31248031828cd5cd71f543dd7937ff746cabe214
Rafael Garcia <rafagarciafdz@gmail.com>
2019-03-04 12:22:45 -0600

    Initial configuration of styles in the blog page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,16 @@ module ApplicationHelper
     end
   end
 
+  def login_blog_helper(style=' ')
+    if current_user.is_a? (GuestUser)
+      (link_to "Login", new_user_session_path, class: style) +
+       " ".html_safe +
+        (link_to "Register", new_user_registration_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
+  end
+
   def source_helper(layout_name)
   	if session[:source]
   		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"

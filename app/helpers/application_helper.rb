ed4fb7e450f553ae2532e968e5bf2bb7427171b5
Rafael Garcia <rafagarciafdz@gmail.com>
2019-03-08 15:36:21 -0600

    Implemented refactor for navigation and the active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,6 @@ module ApplicationHelper
     end
   end
 
-  def login_blog_helper(style=' ')
-    if current_user.is_a? (GuestUser)
-      (link_to "Login", new_user_session_path, class: style) +
-       " ".html_safe +
-        (link_to "Register", new_user_registration_path, class: style)
-    else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    end
-  end
-
   def source_helper(layout_name)
   	if session[:source]
   		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
@@ -29,6 +19,43 @@ module ApplicationHelper
 
   def copyright_generator
     DevcampCopyrightRa::Renderer.copyright 'Rafael García', 'All rights reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'}>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 
 end
